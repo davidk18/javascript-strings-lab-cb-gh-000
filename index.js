d4591be3b41d40b3c1d88c6b31ae03f1967497a0
David Kearney <15169235@studentmail.ul.ie>
2017-11-15 13:36:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
+greetSpecialGuest = 'Hello, ${specialGuest}! High ${3 + 2}!'
+
 var topic = "space";
 
 var conversation = `${topic}`;
